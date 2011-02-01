c51d5a2f3e64c73a6d7d977eb46e6b20f4b2d987
Ademar <ademar@ademar-laptop.(none)>
2011-02-01 22:47:35 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YaffleGuide::Application.config.secret_token = '74be873f1f803bace61efc7a2fd3d177b0bdadaaa41428f71b776e66a2d2eafaf838a37cf4e6e9a29c363c9c96ca94a58fd45b62ff3c4c9e56683cdc0b4c12c4'
